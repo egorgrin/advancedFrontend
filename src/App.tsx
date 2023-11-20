bda60b5e6f9b3c6c3bb375dd479100616efab19e
Egor Grin <35704842+egorgrin@users.noreply.github.com>
2023-11-20 21:28:16 +0500

    Lesson 6: code splitting (lazy, suspense)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import {Counter} from './components/Counter';
 import './index.scss'
+import {Link, Route, Routes} from 'react-router-dom';
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {Suspense} from "react";
 
 
 const App = () => {
   return (
       <div className="app">
-        <Counter />
+        <Link to={'/'}>MainPage</Link>
+        <Link to={'/about'}>AboutPage</Link>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path={'/'} element={<MainPageAsync/>}/>
+            <Route path={'/about'} element={<AboutPageAsync/>}/>
+          </Routes>
+        </Suspense>
+
       </div>
   );
 };
