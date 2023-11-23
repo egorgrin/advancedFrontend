73b295773d9fa7afa150244146cd9c8c12fd250a
Egor Grin <35704842+egorgrin@users.noreply.github.com>
2023-11-23 18:55:51 +0500

    Lesson 7: theme context created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import './index.scss'
+import './styles/index.scss'
 import {Link, Route, Routes} from 'react-router-dom';
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {Suspense} from "react";
-
+import {Suspense, useContext, useState} from "react";
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+  const {theme, toggleTheme} = useTheme()
   return (
-      <div className="app">
+      <div className={`app ${theme}`}>
+        <button onClick={toggleTheme}>Change Theme</button>
         <Link to={'/'}>MainPage</Link>
         <Link to={'/about'}>AboutPage</Link>
         <Suspense fallback={<div>Loading...</div>}>
@@ -16,7 +18,6 @@ const App = () => {
             <Route path={'/about'} element={<AboutPageAsync/>}/>
           </Routes>
         </Suspense>
-
       </div>
   );
 };
--- src/pages/AboutPage/AboutPage.async.tsx
+++ src/pages/AboutPage/AboutPage.async.tsx
@@ -2,5 +2,6 @@ import {lazy} from 'react';
 
 export const AboutPageAsync = lazy(() => new Promise(resolve => {
   //@ts-ignore
+  // Искусственная задержка
   setTimeout(() => resolve(import('./AboutPage')), 1500)
 }));
