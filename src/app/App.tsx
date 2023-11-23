5c351ffc03d0ddf7807165322066acf4302205b3
Egor Grin <35704842+egorgrin@users.noreply.github.com>
2023-11-23 23:47:03 +0500

    Lesson 10: Feature-Sliced Design methodology implemented.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
 import './styles/index.scss'
-import {Link, Route, Routes} from 'react-router-dom';
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {Suspense} from "react";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {Link, Route, Routes} from 'react-router-dom';
+import {classNames} from "shared/lib/classNames/classNames";
+
+import {useTheme} from "app/providers/ThemeProvider/lib/useTheme";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 const App = () => {
   const {theme, toggleTheme} = useTheme()
@@ -15,8 +16,8 @@ const App = () => {
         <Link to={'/about'}>AboutPage</Link>
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
-            <Route path={'/'} element={<MainPageAsync/>}/>
-            <Route path={'/about'} element={<AboutPageAsync/>}/>
+            <Route path={'/'} element={<MainPage />}/>
+            <Route path={'/about'} element={<AboutPage />}/>
           </Routes>
         </Suspense>
       </div>
